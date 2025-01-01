8544d2317d0919bb3dc826a4c160fb207a206866
Adam-Elmi <Adamcade123@gmail.com>
2025-01-01 17:27:38 +0300

    Made Some Changes

--- src/utils/fullYearCycle.js
+++ src/utils/fullYearCycle.js
@@ -1,11 +1,19 @@
-function fullYearCycle(fiftyDays) {
+function fullYearCycle(fiftyDays, leapYear = 0) {
     try {
         const variableName = {fiftyDays};
 
         if(!fiftyDays) {
             throw new Error(`${Object.keys(variableName)[0]} is not found.`)
         }
-        return (fiftyDays * 7) + 15;
+        else if(fiftyDays) {
+            const yearCycle = (fiftyDays * 7) + 15;
+            switch(true) {
+                case yearCycle <= 365 + leapYear:
+                    return yearCycle;
+                default:
+                    throw new Error(`${Object.keys(variableName)[0]} must equal to or less than 365 + leap year.`);
+            }
+        }
     } catch (error) {
         console.error(`Caught an error: ${error.message}`);
     }
