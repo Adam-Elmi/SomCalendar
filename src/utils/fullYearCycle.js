6e4f84c4c0a72271acbd3f7b50bc09175694fa4d
Adam-Elmi <Adamcade123@gmail.com>
2025-01-01 16:53:40 +0300

    Enhanced the code

--- src/utils/fullYearCycle.js
+++ src/utils/fullYearCycle.js
@@ -7,11 +7,11 @@ function fullYearCycle(fiftyDays) {
         if(!fiftyDays) {
             throw new Error(`${Object.keys(variableName)[0]} is not found.`)
         }
-        return (7 * fiftyDays) + 15;
+        return typeof fiftyDays;
     } catch (error) {
         console.error(`Caught an error: ${error.message}`);
     }
 };
 
-const result = fullYearCycle(fiftyDaysCycle(2));
+const result = fullYearCycle(fiftyDaysCycle(0));
 console.log(result);
