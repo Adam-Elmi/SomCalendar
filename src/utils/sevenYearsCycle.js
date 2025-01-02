a91b993a33ecd99307a34b55cfc1e880e4a518e7
Adam-Elmi <Adamcade123@gmail.com>
2025-01-02 14:07:05 +0300

    Made Some Changes

--- src/utils/sevenYearsCycle.js
+++ src/utils/sevenYearsCycle.js
@@ -1,15 +1,12 @@
 function sevenYearsCycle(fullYear) {
     try {
-        const variableName = { fullYear };
-        const varName = Object.keys(variableName)[0];
-
         switch (true) {
             case !fullYear:
-                throw new Error(`${varName} is not found.`);
+                throw new Error("fullYear is not found.");
             case typeof fullYear === "number":
                 return (fullYear * 7);
             default:
-                throw new Error(`${varName} is not a number.`);
+                throw new Error("fullYear is not a number.");
         }
     } catch (error) {
         console.error("Caught an error: ", error.message);
