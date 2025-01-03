a5e030658934cb2ce65d0e9bbed4a09d698527f7
Adam-Elmi <Adamcade123@gmail.com>
2025-01-03 18:49:59 +0300

    Added getFiftyDays method

--- src/utils/somaliDate.js
+++ src/utils/somaliDate.js
@@ -1,24 +1,29 @@
-import { somaliMonths, somaliDays } from "./data.js";
+import { somaliMonths, somaliDays, days } from "./data.js";
+import fiftyDaysCycle from "./fiftyDaysCycle.js";
+import fullYearCycle from "./fullYearCycle.js";
+import sevenYearsCycle from "./sevenYearsCycle.js";
 
 class SomaliDate extends Date {
-    constructor() {
-        super();
+  constructor() {
+    super();
+  }
+  currentMonth() {
+    if (!somaliMonths) {
+      throw new ReferenceError("somaliMonths is not found.");
     }
-    currentMonth() {
-        if(!somaliMonths) {
-           throw new ReferenceError("somaliMonths is not found.");
-        }
-        return somaliMonths[this.getMonth()];
+    return somaliMonths[this.getMonth()];
+  }
+  currentDay() {
+    if (!somaliDays) {
+      throw new ReferenceError("somaliDays is not found.");
     }
-    currentDay() {
-        if(!somaliDays) {
-            throw new ReferenceError("somaliDays is not found.");
-         }
-        return somaliDays[this.getDay()];
-    }
-};
+    return somaliDays[this.getDay()];
+  }
+  getFiftyDays() {
+    return fiftyDaysCycle(days[this.currentDay()]);
+  }
+}
 
 const somaliDate = new SomaliDate();
 
-
-
+console.log(somaliDate.getFiftyDays());
