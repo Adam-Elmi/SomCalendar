3a4d2599474c4f497039e6b37521f0bdf2d7f0c2
Adam-Elmi <Adamcade123@gmail.com>
2025-01-04 23:37:54 +0300

    Fixed a bug

--- src/utils/getCurrentDay.js
+++ src/utils/getCurrentDay.js
@@ -1,10 +1,4 @@
-function getCurrentDay() {
-    const currentDate = new Date();
-    const currentMonth = currentDate.getMonth();
-    const currentYear = currentDate.getFullYear();
-    const months = [];
-    let sumOfDays = 0;
-
+function getMonths(currentYear, currentMonth, months) {
     for (let i = 1; i <= 12; i++) {
         const lastDayOfMonth = new Date(currentYear, currentMonth + i, 0);
         let daysIn = lastDayOfMonth.getDate();
@@ -20,16 +14,37 @@ function getCurrentDay() {
 
         months.push(daysIn);
     }
+}
 
+
+
+function getCurrentDay() {
+    const currentDate = new Date();
+    const currentMonth = currentDate.getMonth();
+    const currentYear = currentDate.getFullYear();
+    const months = [];
+    const previousYearMonths = [];
+    let sumOfDays = 0;
+
+    getMonths(currentYear, currentMonth, months);
+    
     for (let i = 0; i < months.length; i++) {
         if (i === currentMonth + 1) {
             break;
         } else {
+            const previousYear = currentYear - 1;
+            getMonths(previousYear, i, previousYearMonths);
+            const monthsAfterJuly = previousYearMonths.slice(7);
+            const sumOfMonthsAfterJuly = monthsAfterJuly.reduce((initialValue, currentValue) => {
+                return initialValue + currentValue;
+            }, 0);
+
             const previousMonths = months.slice(0, currentMonth);
             const sumOfPreviousMonths = previousMonths.reduce((initialValue, currentValue) => {
                 return initialValue + currentValue;
-            }, 0);         
-            sumOfDays = sumOfPreviousMonths + currentDate.getDate();
+            }, 0);
+
+            sumOfDays = sumOfMonthsAfterJuly + sumOfPreviousMonths + currentDate.getDate();
         }
     }
     console.log(sumOfDays);
