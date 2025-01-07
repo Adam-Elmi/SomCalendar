8d0168508773a481468bcd2d3898fc02a0670cfe
Adam-Elmi <Adamcade123@gmail.com>
2025-01-07 20:22:58 +0300

    Added getYearName method

--- src/utils/somaliDate.js
+++ src/utils/somaliDate.js
@@ -1,5 +1,6 @@
 import { somaliMonths, somaliDays } from "./data.js";
 import getCurrentDay from "./getCurrentDay.js";
+import getYearStart from "./getYearStart.js";
 
 class SomaliDate extends Date {
   constructor() {
@@ -20,10 +21,11 @@ class SomaliDate extends Date {
   getToday() {
     return getCurrentDay(this, this.getFullYear(), this.getMonth());
   }
+  getYearName() {
+    return getYearStart(this.getFullYear());
+  }
 }
 
 const somaliDate = new SomaliDate();
 
-console.log(somaliDate.currentMonth());
-console.log(somaliDate.currentDay());
-console.log(somaliDate.getToday());
+console.log(somaliDate.getYearName());
