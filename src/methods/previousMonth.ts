e90aae51c66b71e03fc48bbf3110eceb6cbf4bd3
Adam-Elmi <Adamcade123@gmail.com>
2025-02-19 20:14:24 +0300

    Removed previousMonth.ts

--- src/methods/previousMonth.ts
+++
@@ -1,6 +0,0 @@
-function previousMonth(currentYear: number, currentMonth: number): number {
-    const prevMonth = new Date(currentYear, currentMonth, 0);
-    return prevMonth.getDate();
-};
-
-export default previousMonth;
