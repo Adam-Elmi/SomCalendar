9c3c00396d47c342a61bc1fd053c727b8542b64d
Adam-Elmi <Adamcade123@gmail.com>
2025-02-19 20:14:21 +0300

    Removed getYearStart.ts

--- src/methods/getYearStart.ts
+++
@@ -1,8 +0,0 @@
-import previousMonth from "./previousMonth.js";
-
-function getYearStart(currentYear: number): number {
-    const startDay = previousMonth(currentYear, 7) - 11;
-    return startDay;
-}
-
-export default getYearStart;
